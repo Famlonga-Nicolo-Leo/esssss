6d7e3c02ed1680461284c9f623f42db6682e8fd9
Famlonga-Nicolo-Leo <famlonga.nicolo@iisgalvanimi.edu.it>
2025-11-09 12:14:48 +0000

    p4

--- famlonga-nicolo-fila_a/src/app/app.ts
+++ famlonga-nicolo-fila_a/src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cassettiera } from './cassettiera/cassettiera';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Cassettiera],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('famlonga-nicolo-fila_a');
 
-  
+  }
--- famlonga-nicolo-fila_a/src/app/casella-posta/casella-posta.ts
+++ famlonga-nicolo-fila_a/src/app/casella-posta/casella-posta.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-casella-posta',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './casella-posta.css',
 })
 export class CasellaPosta {
-
+  @Input() utente!: string;
+    ngOnInit() {}
 }
